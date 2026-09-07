d71a0332b8cc0f883a2874afb6f1bd6be703c5ea
Abodabodd <122629521+Abodabodd@users.noreply.github.com>
2026-09-07 14:08:28 +0300

    Add mainUrl and name properties to eishk class

--- 3isk/src/main/kotlin/com/eshk/eishk.kt
+++ 3isk/src/main/kotlin/com/eshk/eishk.kt
@@ -14,6 +14,7 @@ import kotlin.io.encoding.Base64
 import android.service.controls.ControlsProviderService.TAG
 import com.lagradost.cloudstream3.syncproviders.providers.OpenSubtitlesApi.Companion.headers
 
+
 class eishk : MainAPI() {
     override var mainUrl = "https://3esk.onl"
     override var name = "قصة عشق"
