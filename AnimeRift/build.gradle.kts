d65e252d714851a4674b48d83e6793d8ddae1a30
Abodabodd <122629521+Abodabodd@users.noreply.github.com>
2026-09-11 01:00:41 +0300

    Add files via upload

---
+++ AnimeRift/build.gradle.kts
@@ -0,0 +1,17 @@
+version = 1
+
+cloudstream {
+    description = ""
+    authors = listOf("Abodabodd")
+    language = "ar"
+
+    status = 1
+
+    tvTypes = listOf(
+        "TvSeries",
+        "Anime",
+        "Movie"
+    )
+
+    iconUrl = ""
+}
